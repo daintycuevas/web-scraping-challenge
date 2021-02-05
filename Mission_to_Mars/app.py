c434a2b6998b9907d0a45b2af844f428828eecf7
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-04 23:54:48 -0500

    update app

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
-from scrape_mars import scrape_nasa
-import pymongo
-import scrape_mars
+from flask import Flask, render_template, redirect
+from flask_pymongo import pymongo
+from scrape_mars import scrape_info
+
 
 ###############p##################################
 # Flask Setup
 #################################################
+
 app = Flask(__name__)
 
-conn = 'mongodb://localhost:27017/marsdb'
-
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_mission_app")
 
 # #################################################
 # # Flask Routes
@@ -20,18 +17,15 @@ client = pymongo.MongoClient(conn)
 
 @app.route("/")
 def index():
-    mars = client.db.mars.find_one()
+    mars = mongo.db.collection.find_one()
     return render_template("index.html", mars=mars)
 
 
 @app.route("/scrape")
 def scrape():
-    mars = client.db.mars
-    mars.drop()
-    mars_data = scrape_mars.scrape_nasa()
-    print (mars_data)
-    mars.insert(mars_data)
-    return "scrape done!"
+    mars_data = scrape_mars.scrape_info()
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    return redirect("/")
     
 
 if __name__ == '__main__':
