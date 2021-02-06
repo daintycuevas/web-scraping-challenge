ec9380d0c5b70a5689cec2f6eca1d174bd160741
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-02-06 01:08:34 -0500

    add template folder

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -21,7 +21,7 @@ def index():
     return render_template("index.html", mars=mars)
 
 
-@app.route("/scrape")
+@app.route("/scrape_mars")
 def scrape():
     mars_data = scrape_mars.scrape_info()
     mongo.db.collection.update({}, mars_data, upsert=True)
